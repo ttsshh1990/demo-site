e951cff2ec5c0cf9067bdfcb8b03441c437c1670
ttian <ttian@irobot.com>
2017-06-27 14:07:01 -0400

    stylesheets

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :rvm_use_path, '/etc/profile.d/rvm.sh'
 
 # shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # set :shared_dirs, fetch(:shared_dirs, []).push('somedir')
-# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
